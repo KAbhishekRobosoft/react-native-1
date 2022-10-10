1078e07e9bef69918653336bbed0eabf5ca1bd1b
KAbhishekRobosoft <113020229+KAbhishekRobosoft@users.noreply.github.com>
2022-10-10 21:51:14 +0530

    Add files via upload

---
+++ src/components/Button.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Pressable,View,Text,StyleSheet } from 'react-native'
+
+function Button({name,onPress}) {
+  return (
+    <View style= {styles.butCon}>
+        <Pressable style= {styles.but} onPress= {onPress}>
+            <Text style={styles.butInp}>{name}</Text>
+        </Pressable>
+    </View>
+  )
+}
+
+const styles= StyleSheet.create({
+    butCon:{
+            backgroundColor:"orange",
+            padding:10,
+        },
+
+        but:{
+            width:60,
+            height:25  
+        },
+
+        butInp:{
+            textAlign:"center",
+            fontSize:18
+        }
+    })
+export default Button
