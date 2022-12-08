a2543482363b7ef7f1986b7def9ad6b6653d9236
KAbhishekRobosoft <113020229+KAbhishekRobosoft@users.noreply.github.com>
2022-12-08 21:30:40 +0530

    Delete RidersApp directory

--- RidersApp/src/screens/BookingSuccessScreen.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Success } from '../components/SuccessComponent';
-
-export const BookingSuccess = ({navigation}) => {
-  return (
-    <Success greet='Congratulations!' text2='Your booking has been confirmed' onPress={()=>navigation.navigate('BottomTabLoginNavigation')} Press={()=>navigation.goBack()}/>
-  );
-};
--- RidersApp/src/screens/TripJoingSuccessScreen.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Success } from '../components/SuccessComponent';
- 
-export const JoiningTrip = () => {
-  return (
-    <Success greet= 'Congatulations!' text2='You have joined a trip' />
-  );
-};
